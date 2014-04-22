9d3d8f625977c49e910f73ef48fe9c00ee4fbf4e
DNSH <dinesh.ramdhayan@gmail.com>
2014-04-22 18:41:25 -0400

    added pointer code and directory

---
+++ pointers/pointers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	int i = 20, j = 30;
+
+	int *p, *q;
+
+	p = &i;
+	q = &j;
+
+	printf("The value at p is %d\n", *p);
+    printf("The value at q is %d\n", *q);
+
+	printf("Changing values\n");
+
+	*p = 40;
+	
+    printf("The value at p is %d\n", *p);
+
+	return 0;
+}
