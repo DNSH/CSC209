057a3bdbe039845989781898ad0df23e9d3a24f3
DNSH <dinesh.ramdhayan@gmail.com>
2014-04-24 22:48:54 -0400

    more exam code

---
+++ tests/2012F/q5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFSIZE 256
+
+char * fgets_new(char *s, int n, FILE *stream) {
+	
+	int i;
+	char c;
+
+	for (i=0; i < n; i++) {
+		c = getc(stream);
+		s[i] = c;
+	}
+
+	//TODO: handle EOF and \n cases
+
+	return s;
+
+}
+
+int main(void) {
+	
+	int n = BUFSIZE;
+	char *s = malloc(BUFSIZE * sizeof(char));
+	FILE *stream;
+
+	stream = fopen("testfile", "r");
+
+	fgets_new(s, n, stream);
+
+	printf("The value of s is %s\n", s);
+
+	return 0;
+}
